a649b70d5161d8cc611985b47157889fa5f5ac92
Kapil Kumar <kapilkumar.v@vvdntech.in>
2020-10-02 12:43:28 +0530

    UnhandledPromiseRejectionWarning

--- routes/api.js
+++ routes/api.js
@@ -5,23 +5,25 @@ const {getMetadata} = require('page-metadata-parser');
 const domino = require('domino');
 const fetch = require("node-fetch");
 const NodeCache = require( "node-cache" );
-const myCache = new NodeCache( { stdTTL: 60*60, checkperiod: 30*60,maxKeys:200 } );
+const validUrl = require('valid-url');
 
-const stringIsAValidUrl = (s) => {
-  try {
-    new URL(s);
-    return true;
-  } catch (err) {
-    return false;
-  }
-};
+const myCache = new NodeCache( { stdTTL: 60*60, checkperiod: 30*60,maxKeys:200 } );
 
 const inputValidation = require('./variables/input_validation');
 const send_response = require('../send_response');
 const logger = require('../logger');
 
-
 let router = express.Router();
+
+const stringIsAValidUrl = (s) => {
+  if (validUrl.isUri(s)){
+    return true;
+  } 
+  else {
+    return false;
+  }
+};
+
 
 
 /**
@@ -47,18 +49,28 @@ router.post("/getMetaInfo",validate({body: inputValidation.validateURL}), async 
     if ( value == undefined ){
       logger.info("data not exist in myCache");
       // handle miss!
-      const response = await fetch(url);
-      const html = await response.text();
-      const doc = domino.createWindow(html).document;
-      const metadata = getMetadata(doc, url);
-      logger.info("set data in myCache");
-      myCache.set( url, metadata);
-      return send_response.sendResponse(req,res, {
-        status: true,
-        msg: 'Success',
-        data: metadata,
-        error: ""
-      }, 200);
+      try {
+        const response = await fetch(url);
+        const html = await response.text();
+        const doc = domino.createWindow(html).document;
+        const metadata = getMetadata(doc, url);
+        logger.info("set data in myCache");
+        myCache.set( url, metadata);
+        return send_response.sendResponse(req,res, {
+          status: true,
+          msg: 'Success',
+          data: metadata,
+          error: ""
+        }, 200);  
+      } catch (error) {
+        return send_response.sendResponse(req,res, {
+          status: false,
+          msg: 'Something broke!',
+          data: "",
+          error:error.message
+        }, 500);  
+      }
+      
     } else {
       logger.info("data exist in myCache");
       return send_response.sendResponse(req,res, {
